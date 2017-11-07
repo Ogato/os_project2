09d71c2a7be8c3512490e9d4107086e9bbb6f9ed
oscreader <oscreader@OSC>
2017-11-07 14:52:39 -0500

    Added comments and printing of flag names

--- pid_info.c
+++ pid_info.c
@@ -4,28 +4,129 @@
 #include <linux/moduleparam.h>
 #include <linux/kernel.h>
 
+/* Initalize default module parameters */
 static int pid = 1;
 static char* module_name = "pid_info";
 module_param(pid, int, 0666);
 module_param(module_name, charp, 0666);
 MODULE_PARM_DESC(pid, "Process ID");
 
+/* This module takes a process's id and displays its gid, uid,
+ * children and their pids and names
+*/
 static int pid_info_init(void){
-	printk(KERN_NOTICE"Loading Module");
+
+	/* Pointer for iterating through tasks list */
+	struct task_struct* pos;
+
+	/* Pointer for iterating through children list */
+	struct task_struct* child;
+	
+	/* Set flags */
+	int flags;
+
+	/* Loop through the tasks if current is not the process requested */
 	if(current->pid != pid){
-		printk(KERN_NOTICE"Finding Task");
-        	struct task_struct* pos;
         	list_for_each_entry(pos, &(current->tasks), tasks){
                 	if(pos->pid == pid){
-                        	printk(KERN_NOTICE"PID: %d\nUID: %d\nGID: %d\nParentID: %d\nName: %s\nFlags: %d\nChildren:\n",pos->pid,
+				/* Display process information */
+                        	printk(KERN_NOTICE"PID: %d\nUID: %d\nGID: %d\nParentID: %d\nName: %s\nFlags:\n",pos->pid,
 									     				pos->cred->uid,
                                 	    	        	                                        pos->cred->gid,
                                         		                                                pos->real_parent->pid,
-													pos->real_parent->comm,
-													pos->flags);
-			        struct task_struct*  child;
+													pos->real_parent->comm);
+				/* Display set flag names */
+				flags = pos->flags;
+				if(flags & 0x4){
+					printk(KERN_NOTICE"\tPF_EXITING\n");
+				}
+				if(flags & 0x8){
+					printk(KERN_NOTICE"\tPF_EXITPIDONE\n");
+				}
+				if(flags & 0x10){
+					printk(KERN_NOTICE"\tPF_VCPU\n");
+				}
+				if(flags & 0x20){
+					printk(KERN_NOTICE"\tPF_WQ_WORKER\n");
+				}
+				if(flags & 0x40){
+					printk(KERN_NOTICE"\tPF_FORKNOEXEC\n");
+				}
+				if(flags & 0x80){
+					printk(KERN_NOTICE"\tPF_MCE_PROCESS\n");
+				}
+				if(flags & 0x100){
+					printk(KERN_NOTICE"\tPF_SUPERPRIV\n");
+				}
+				if(flags & 0x200){
+					printk(KERN_NOTICE"\tPF_DUMPCORE\n");
+				}
+				if(flags & 0x400){
+					printk(KERN_NOTICE"\tPF_SIGNALED\n");
+				}
+				if(flags & 0x800){
+					printk(KERN_NOTICE"\tPF_MEMALLOC\n");
+				}
+				if(flags & 0x1000){
+					printk(KERN_NOTICE"\tPF_NPROC_EXCEEDED\n");
+				}
+				if(flags & 0x2000){
+					printk(KERN_NOTICE"\tPF_USED_MATH\n");
+				}
+				if(flags & 0x4000){
+					printk(KERN_NOTICE"\tPF_USED_ASYNC\n");
+				}
+				if(flags & 0x8000){
+					printk(KERN_NOTICE"\tPF_NOFREEZE\n");
+				}
+				if(flags & 0x10000){
+					printk(KERN_NOTICE"\tPF_FROZEN\n");
+				}
+				if(flags & 0x20000){
+					printk(KERN_NOTICE"\tPF_FSTRANS\n");
+				}
+				if(flags & 0x40000){
+					printk(KERN_NOTICE"\tPF_KSWAPD\n");
+				}
+				if(flags & 0x80000){
+					printk(KERN_NOTICE"\tPF_MEMALLOC_NOIO\n");
+				}
+				if(flags & 0x100000){
+					printk(KERN_NOTICE"\tPF_LESS_THROTTLE\n");
+				}
+				if(flags & 0x200000){
+					printk(KERN_NOTICE"\tPF_KTHREAD\n");
+				}
+				if(flags & 0x400000){
+					printk(KERN_NOTICE"\tPF_RANDOMIZE\n");
+				}
+				if(flags & 0x800000){
+					printk(KERN_NOTICE"\tPF_SWAPWRITE\n");
+				}
+				if(flags & 0x1000000){
+					printk(KERN_NOTICE"\tPF_SPREAD_PAGE\n");
+				}
+				if(flags & 0x2000000){
+					printk(KERN_NOTICE"\tPF_SPREAD_SLAB\n");
+				}
+				if(flags & 0x4000000){
+					printk(KERN_NOTICE"\tPF_NO_SETAFFINITY\n");
+				}
+				if(flags & 0x8000000){
+					printk(KERN_NOTICE"\tPF_MCE_EARLY\n");
+				}
+				if(flags & 0x20000000){
+					printk(KERN_NOTICE"\tPF_MUTEX_TESTER\n");
+				}
+				if(flags & 0x40000000){
+					printk(KERN_NOTICE"\tPF_FREEZER_SKIP\n");
+				}
+				if(flags & 0x80000000){
+					printk(KERN_NOTICE"\tPF_SUSPEND_TASK\n");
+				}
+				printk(KERN_NOTICE"Children:\n");
         			list_for_each_entry(child, &(pos->children), sibling){
-                			printk(KERN_NOTICE"\tChild PID:%d\nName: %s\n",child->pid, child->comm);
+                			printk(KERN_NOTICE"\tChild PID:%d\n\tName: %s\n",child->pid, child->comm);
        				 }
 
                         	break;
@@ -33,16 +134,96 @@ static int pid_info_init(void){
         	}
 
 	}
+
+	/* Access informaiton directly from current */
 	else{
-		printk(KERN_NOTICE"PID: %d\nUID: %d\nGID: %d\nParentID: %d\nParent Name: %s\nFlags: %d\nChildren:\n",current->pid,
+		printk(KERN_NOTICE"PID: %d\nUID: %d\nGID: %d\nParentID: %d\nParent Name: %s\nFlags:\n",current->pid,
 											current->cred->uid,
                                                                                		current->cred->gid,
                                        			                      		current->real_parent->pid,
-											current->real_parent->comm,
-											current->flags);
-	        struct task_struct*  child;
+		/* Display set flag name */				       current->real_parent->comm);
+		flags = current->flags;
+                if(flags & 0x4){
+                	printk(KERN_NOTICE"\tPF_EXITING\n");
+                }
+                if(flags & 0x8){
+                        printk(KERN_NOTICE"\tPF_EXITPIDONE\n");
+                }
+                if(flags & 0x10){
+                        printk(KERN_NOTICE"\tPF_VCPU\n");
+                }
+                if(flags & 0x20){
+                        printk(KERN_NOTICE"\tPF_WQ_WORKER\n");
+                }
+                if(flags & 0x40){
+                	printk(KERN_NOTICE"\tPF_FORKNOEXEC\n");
+                }
+                if(flags & 0x80){
+                        printk(KERN_NOTICE"\tPF_MCE_PROCESS\n");
+                }
+		if(flags & 0x100){
+                        printk(KERN_NOTICE"\tPF_SUPERPRIV\n");
+                }
+                if(flags & 0x200){
+                        printk(KERN_NOTICE"\tPF_DUMPCORE\n");
+                }
+                if(flags & 0x400){
+                        printk(KERN_NOTICE"\tPF_SIGNALED\n");
+                }
+		if(flags & 0x800){
+                        printk(KERN_NOTICE"\tPF_MEMALLOC\n");
+                }
+                if(flags & 0x1000){
+                        printk(KERN_NOTICE"\tPF_NPROC_EXCEEDED\n");
+                }
+                if(flags & 0x2000){
+                        printk(KERN_NOTICE"\tPF_USED_MATH\n");
+                }
+                if(flags & 0x4000){
+                        printk(KERN_NOTICE"\tPF_USED_ASYNC\n");
+                }
+                if(flags & 0x8000){
+                        printk(KERN_NOTICE"\tPF_NOFREEZE\n");
+                }
+		if(flags & 0x10000){
+                        printk(KERN_NOTICE"\tPF_FROZEN\n");
+                }
+                if(flags & 0x20000){
+                        printk(KERN_NOTICE"\tPF_FSTRANS\n");
+                }
+                if(flags & 0x40000){
+                        printk(KERN_NOTICE"\tPF_KSWAPD\n");
+                }
+                if(flags & 0x80000){
+                        printk(KERN_NOTICE"\tPF_MEMALLOC_NOIO\n");
+                }
+                if(flags & 0x100000){
+                        printk(KERN_NOTICE"\tPF_LESS_THROTTLE\n");
+                }
+                if(flags & 0x200000){
+                        printk(KERN_NOTICE"\tPF_KTHREAD\n");
+            	}
+		if(flags & 0x400000){
+                        printk(KERN_NOTICE"\tPF_RANDOMIZE\n");
+                }
+                if(flags & 0x800000){
+                        printk(KERN_NOTICE"\tPF_SWAPWRITE\n");
+                }
+                if(flags & 0x1000000){
+                        printk(KERN_NOTICE"\tPF_SPREAD_PAGE\n");
+                }
+                if(flags & 0x2000000){
+                        printk(KERN_NOTICE"\tPF_SPREAD_SLAB\n");
+                }
+                if(flags & 0x4000000){
+                        printk(KERN_NOTICE"\tPF_NO_SETAFFINITY\n");
+                }
+		if(flags & 0x80000000){
+                        printk(KERN_NOTICE"\tPF_SUSPEND_TASK\n");
+                }
+                printk(KERN_NOTICE"Children:\n");
         	list_for_each_entry(child, &(current->children), sibling){
-                	printk(KERN_NOTICE"\tChild PID:%d\nChild Name: %s\n",child->pid, child->comm);
+                	printk(KERN_NOTICE"\tChild PID:%d\n\tChild Name: %s\n",child->pid, child->comm);
         	}
 	}
 
--- pid_vminfo.c
+++ pid_vminfo.c
@@ -4,20 +4,28 @@
 #include <linux/proc_fs.h>
 #include <linux/sched.h>
 
+/* This module displays the state of the current process's VMA 
+ * through a proc file named vm_proc
+*/
 static int vm_proc_show(struct seq_file *f){
+
+	/* Variable to iterate over the current process's VMAs */
+	struct vm_area_struct* vms;
+
 	seq_printf(f, "PID: %d\nName: %s\nVM_AREAS:\n", current->pid,
 					     		current->comm);
-
-	struct vm_area_struct* temp = current->mm->mmap;
-	while(temp != NULL){
-		seq_printf(f, "Start Address: %lu\nEnd Address: %lu\nFlags: %lu\n",temp->vm_start,
-								     temp->vm_end,
-								     temp->vm_flags);
-		temp = temp->vm_next;
+	/* Loop through list of VMAs until the end */
+	vms = current->mm->mmap;
+	while(vms != NULL){
+		seq_printf(f, "Start Address: %lu\nEnd Address: %lu\nFlags: %lu\n",vms->vm_start,
+								     vms->vm_end,
+								     vms->vm_flags);
+		vms = vms->vm_next;
 	}
 	return 0;
 }
 
+/* Open a new proc file when requested */
 static int vm_proc_open(struct inode* inode, struct file* file){
 	return single_open(file, vm_proc_show, NULL);
 }
@@ -30,16 +38,19 @@ static const struct file_operations vm_proc_fops = {
 	.release = single_release,
 };
 
+/* Create the proc file when module is loaded */ 
 static int __init vm_proc_init(void){
 	proc_create("vm_proc", 0, NULL, &vm_proc_fops);
 	return 0;
 }
 
+/* Remove the proc file when the module is removed */
 static void __exit vm_proc_exit(void){
 	remove_proc_entry("vm_proc", NULL);
 }
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Group 16");
 module_init(vm_proc_init);
 module_exit(vm_proc_exit);
 
